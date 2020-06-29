cbbeffb5469062cda6f095d6aba8c8411ebc0807
EngrAhsanShahid <EngrAhsanShahid@users.noreply.github.com>
2020-06-29 11:38:57 +0500

    add

--- js/app.js
+++ js/app.js
@@ -1,13 +1,27 @@
+var value;
 function getValue(value){
-    var value = document.getElementById("initial_text").innerHTML += value;
+    value = document.getElementById("initial_text").innerHTML += value;
 }
 function clearAll(){    
-    document.getElementById("initial_text").innerHTML = "";
-    document.getElementById("result").innerHTML = "";
+    document.getElementById("initial_text").innerHTML = " ";
+    document.getElementById("result").innerHTML = " ";
 }
 function getResult(){
     var num = document.getElementById("initial_text").innerHTML;
     var result = eval(num);
-    console.log(result)
     document.getElementById("result").innerHTML = result;
+}
+function sqRoot(value){
+    value = document.getElementById("initial_text").innerHTML += value;
+    var split = value.split('s');
+    var sqroot = Math.sqrt(split[0]);
+    document.getElementById("result").innerHTML = sqroot;
+}
+function callDivide(value){
+    value = document.getElementById("initial_text").innerHTML += value;
+    var split = value.split('1/x');
+    console.log(split)
+    var divide = 1/split[0];
+    console.log(divide)
+    document.getElementById("result").innerHTML = divide;
 }
